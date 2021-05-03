60d097ebc1b9db50bc99cebbd050687e09427928
Leonardo Fragozo <lqfragozo@gmail.com>
2021-05-03 08:42:35 -0300

    Leo's first gem

---
+++ lib/study_algorithm.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "study_algorithm/version"
+
+module StudyAlgorithm
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/study_algorithm_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe StudyAlgorithm do
+  it "has a version number" do
+    expect(StudyAlgorithm::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
